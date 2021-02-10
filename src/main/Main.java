94c7812f19c7799813edc973a2071cac0e1fcdc0
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-02-10 15:00:42 +0700

    thêm hàm tính hiệu 2 số

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		int first = 3, second =4;
 		Caculator caculator = new Caculator();
-		System.out.print("tong = "+ caculator.add(first,second));
+		System.out.println("tong = "+ caculator.add(first,second));
+		System.out.println("Hieu: "+caculator.subtract(first, second));
 	}
 
 }
