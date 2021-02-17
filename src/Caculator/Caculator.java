653dd2eb710ca78e196b2e85c1196f040171b0e6
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-02-17 13:31:59 +0700

    Bổ sung hàm nhân và chia 2 số

--- src/Caculator/Caculator.java
+++ src/Caculator/Caculator.java
@@ -7,4 +7,10 @@ public class Caculator {
 	public int subtract(int first, int second) {
 		return first - second;
 	}
+	public double multiply(int first, int second) {
+		return first *second;
+	}
+	public double divide(int first, int second) {
+		return (double)first/second;
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		Caculator caculator = new Caculator();
 		System.out.println("tong = "+ caculator.add(first,second));
 		System.out.println("Hieu: "+caculator.subtract(first, second));
+		System.out.println("Tich: "+caculator.multiply(first, second));
+		System.out.println("Thuong: "+caculator.divide(first, second));
 	}
 
 }
