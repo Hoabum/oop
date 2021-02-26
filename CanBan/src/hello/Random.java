7249cb5df5dd7d95bee9123bd580383d5ccfdd00
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-02-26 12:38:17 +0700

    #randomNumber

---
+++ CanBan/src/hello/Random.java
@@ -0,0 +1,18 @@
+package hello;
+
+class variable {
+
+	public static void main(String[] args) {
+		int age = 21;// khai bao tuoi
+		
+		char name;
+		final int  year =2001;
+		double var4 = year + (double)age;
+		int max =100;
+		int min =1;
+		int range =(max-min)+1;
+		int randomNumber = (int)(Math.random()*range )+min;
+		System.out.println("random: "+randomNumber );
+		}
+
+}
