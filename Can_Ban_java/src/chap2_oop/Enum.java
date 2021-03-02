a4b65135a36189cb43e14d703bc8c0e44eb176d7
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-03-02 15:11:21 +0700

    #Enum

---
+++ Can_Ban_java/src/chap2_oop/Enum.java
@@ -0,0 +1,14 @@
+package chap2_oop;
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+public class Enum {
+		  public void main(String[] args) { 
+		    Level myVar = Level.MEDIUM; 
+		    System.out.println(myVar); 
+		  } 
+}
+
