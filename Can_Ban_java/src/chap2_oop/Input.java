0a84d6fe64494d6aca320e08fac902f4a8757ad6
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-03-02 13:46:29 +0700

    #Input

---
+++ Can_Ban_java/src/chap2_oop/Input.java
@@ -0,0 +1,17 @@
+package chap2_oop;
+
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		int msv = sc.nextInt();
+		System.out.println(" My name is  "+ name);
+		System.out.println("MSV :"+ msv);
+
+	}
+
+}
