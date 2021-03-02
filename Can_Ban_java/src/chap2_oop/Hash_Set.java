d69c280ab110fc81b477f2a44dbeebf06342897f
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-03-02 17:04:28 +0700

    #Hash_Set

---
+++ Can_Ban_java/src/chap2_oop/Hash_Set.java
@@ -0,0 +1,15 @@
+package chap2_oop;
+
+import java.util.HashSet;
+
+public class Hash_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String>Cart = new HashSet<String>();
+		Cart.add("mess");
+		Cart.add("ot");
+		System.out.println(Cart);
+	}
+
+}
