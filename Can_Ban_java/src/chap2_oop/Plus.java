61615f15fa58a6d47a721c78d35b77552bd895f1
nguyenthanhhoa2509 <hoabumtyn2509@gmail.com>
2021-03-02 15:02:51 +0700

    #1+2...n

---
+++ Can_Ban_java/src/chap2_oop/Plus.java
@@ -0,0 +1,19 @@
+package chap2_oop;
+
+public class Plus {
+
+	public static void main(String[] args) {
+		int result = sum(1,10);
+		System.out.println("sum= "+ result);
+	}
+	public static int sum(int start, int end)
+	{
+		if(end > start)
+		{
+			return end+sum(start,end-1);
+		}
+		else {
+			return end;
+		}
+	}
+}
